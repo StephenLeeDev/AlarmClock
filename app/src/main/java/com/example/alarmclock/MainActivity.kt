eec26cda325990a527db3736c6fd8b4331ab7427
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-08-21 00:00:24 +0900

    ADD | Set Alarm Time Feature Implementation

--- app/src/main/java/com/example/alarmclock/MainActivity.kt
+++ app/src/main/java/com/example/alarmclock/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.alarmclock
 
+import android.app.TimePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Step1 Initialize Views
+        initOnOffButton()
+        initChangeAlarmTimeButton()
+
+        // Step2 Get Data
+
+        // Step3 Display Data
+    }
+
+    private fun initChangeAlarmTimeButton() {
+        val onOffButton = findViewById<Button>(R.id.onOffButton)
+        onOffButton.setOnClickListener {
+
+            val calendar = Calendar.getInstance()
+
+            TimePickerDialog(this, { picker, hour, minute ->
+
+                val model = saveAlarmModel(
+                    hour = hour,
+                    minute = minute,
+                    false
+                )
+
+            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show()
+
+        }
+    }
+
+    private fun initOnOffButton() {
+        val changeAlarmTimeButton = findViewById<Button>(R.id.changeAlarmTimeButton)
+        changeAlarmTimeButton.setOnClickListener {
+
+        }
+    }
+
+    private fun saveAlarmModel(
+        hour: Int,
+        minute: Int,
+        onOff: Boolean
+    ): AlarmDisplayModel {
+
+        val model = AlarmDisplayModel(
+            hour = hour,
+            minute = minute,
+            false
+        )
+
+        val sharedPreferences = getSharedPreferences("time", Context.MODE_PRIVATE)
+
+        with(sharedPreferences.edit()) {
+            putString("alarm", model.makeDataForDB())
+            putBoolean("onOff", model.onOff)
+            commit()
+        }
+
+        return model
     }
 }
